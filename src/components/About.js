cd875573afb6a7f3ed82934147a261b8f41244d2
Camo2243 <106394864+Camo2243@users.noreply.github.com>
2022-05-27 10:32:29 -0500

    Add files via upload
    Se termina de añadir información

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 <div className='col-2'>
                     <h2>CONOCEME</h2>
                     <span className='line'></span>
-                    <p>Soy un tipo de trabajador que está acostumbrado al trabajo bajo presión, tengo varios años de experiencia en atención al cliente y en búsqueda de oportunidades comerciales. También me considero una persona resolutiva, con buen ánimo y capaz de resolver problemas fácilmente.</p>
+                    <p>-Soy un tipo de trabajador que está acostumbrado al trabajo bajo presión, tengo varios años de experiencia en atención al cliente y en búsqueda de oportunidades comerciales. También me considero una persona resolutiva, con buen ánimo y capaz de resolver problemas fácilmente.</p>
                     
                     
                 </div>
